cacc7f5312fe697c7881a072f343948d4d97603b
ManashSwain <manash0802@gmail.com>
2026-03-07 22:35:43 +0530

    Implement routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-
+import { Routes, Route } from "react-router";
+import Rootlayout from "./layouts/Rootlayout";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+       <Routes>
+      <Route path="/" element={<Rootlayout />} />
+    </Routes>
     </>
   );
 }
