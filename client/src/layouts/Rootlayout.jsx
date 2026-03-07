30e4cd37cd1130e2cfb208b24aadae120bc50399
ManashSwain <manash0802@gmail.com>
2026-03-07 22:06:28 +0530

    Implement Rootlayout ui design

---
+++ client/src/layouts/Rootlayout.jsx
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const Rootlayout = () => {
+  return (
+   <>
+   <Navbar/>
+   <Footer/>
+   </>
+  )
+}
+
+export default Rootlayout
