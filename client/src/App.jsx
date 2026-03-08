7d107d10424d27f6199159204f330b29b024f6e3
ManashSwain <manash0802@gmail.com>
2026-03-08 10:48:51 +0530

    Implement homepage structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router";
 import Rootlayout from "./layouts/Rootlayout";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Rootlayout />}></Route>
+        <Route path="/" element={<Rootlayout />}>
+        <Route path="/" element={<Homepage/>}/>
+        </Route>
       </Routes>
     </>
   );
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Promosection from '../components/Promosection'
+import Collections from '../components/Collections'
+import Productlist from '../components/Productlist'
+
+const Homepage = () => {
+  return (
+   <>
+   <Promosection/>
+   <Collections/>
+   <Productlist/>
+   </>
+  )
+}
+
+export default Homepage
