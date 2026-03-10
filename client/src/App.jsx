38c212889521197d10fe7b1d1fac6bb8012a2cae
ManashSwain <manash0802@gmail.com>
2026-03-10 21:38:20 +0530

    Implement route changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Rootlayout from "./layouts/Rootlayout";
 import Homepage from "./pages/Homepage";
 import Categoriespage from "./pages/Categoriespage";
 import Categoryproducts from "./pages/Categoryproducts";
+import Productdetail from "./components/Productdetail";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
         <Route path="/" element={<Rootlayout />}>
           <Route path="/" element={<Homepage />} />
           <Route path="categories">
-          <Route index element={<Categoriespage/>}/>
-          <Route path=":categoryname" element={<Categoryproducts/>}/>
+            <Route index element={<Categoriespage />} />
+            <Route path=":categoryname" element={<Categoryproducts />}></Route>
           </Route>
+          <Route path="product/:productId" element={<Productdetail />} />
         </Route>
       </Routes>
     </>
