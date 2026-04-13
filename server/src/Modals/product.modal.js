ce65b9ea3bb131752343581d51550bfefd4ffb11
ManashSwain <manash0802@gmail.com>
2026-04-13 23:14:08 +0530

    Implement productmodal

--- server/src/Modals/product.modal.js
+++ server/src/Modals/product.modal.js
@@ -1,17 +1,58 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name : {
-    type : String,
-    required : true
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    subcategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subcategory",
+      required: true,
+    },
+    images: [{ type: String }],
+    slug: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
   },
-  description : {
-    type : String,
-    required : true
-  },
-  subdescription : {
-    type : String,
-    required : true
-  },
-  
-},{timestamps : true})
+  { timestamps: true },
+);
+
+export const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
