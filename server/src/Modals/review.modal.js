ea7c9eeb7666f88948acf28d2781395d2a5acb77
ManashSwain <manash0802@gmail.com>
2026-04-16 00:16:48 +0530

    Implement review moal

---
+++ server/src/Modals/review.modal.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required : true
+    },
+    clerkId : {
+        type : String,
+        required : true
+    },
+    productId : {
+       type : mongoose.Schema.Types.ObjectId,
+       ref : "Product"
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    content : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        required : true
+    }
+},{timestamps : true})
+
+export const Review = mongoose.models.Review || mongoose.model("Review", reviewSchema)
