c5ef39b584725ab2053dfbdd85e09867ae12b041
ManashSwain <manash0802@gmail.com>
2026-04-17 00:17:54 +0530

    Implement review route

--- server/src/Routes/review.route.js
+++ server/src/Routes/review.route.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { createReview, deleteReview, getReview, updateReview } from "../Controllers/review.controller.js";
 
 const router = express.Router();
 
-router.post("/createreview",);
-router.get("/getreviews",);
-router.patch("/updatereview/:reviewId",);
-router.delete("deletereview/:reviewId",);
+router.post("/createreview",createReview);
+router.get("/getreviews",getReview);
+router.patch("/updatereview/:reviewId",updateReview);
+router.delete("deletereview/:reviewId",deleteReview);
 
 export default router
