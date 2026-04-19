4f2ebb9ace489d031c51176cbfbf96fc9dc6b811
ManashSwain <manash0802@gmail.com>
2026-04-19 23:27:10 +0530

    Implement address structure

---
+++ server/src/Controllers/address.controller.js
@@ -0,0 +1,7 @@
+// Add Address (post)
+export const addAddress = async (req,res)=>{
+
+}
+// Get Address (get)
+// Update Address (patch)
+// Delete Address (delete)
--- server/src/Modals/address.modal.js
+++ server/src/Modals/address.modal.js
@@ -1,7 +1,54 @@
 import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-    
+    clerkId : {
+        type : String,
+        required : true
+    },
+    fullName : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : String,
+        required : true
+    },
+    addressLine1 : {
+        type : String,
+        required : true
+    },
+    addressLine2 : {
+        type : String
+    },
+    country : {
+        type : String,
+        required : true
+    },
+    state : {
+        type : String,
+        required : true
+    },
+    district : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String ,
+        required : true
+    },
+    pincode : {
+        type : String,
+        required : true
+    },
+    landmark : {
+        type: String,
+        required : true
+    },
+    addressType : {
+        type : String,
+        enum : ["home", "work", "other"],
+        default : "home"
+    }
 },{timestamps : true})
 
 export const Address = mongoose.models.Address || mongoose.model("Address", addressSchema)
