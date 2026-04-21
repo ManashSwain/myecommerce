31f6f21f924034bfb8ec86692bbb7b0bbdb8a70b
ManashSwain <manash0802@gmail.com>
2026-04-21 23:21:30 +0530

    Implement react router dom

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import * as React from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
