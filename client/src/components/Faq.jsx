1038c5b33aa10abbfb675c4b53681f0c61164dca
ManashSwain <manash0802@gmail.com>
2026-09-23 17:35:08 +0530

    Update FAQ stylings

--- client/src/components/Faq.jsx
+++ client/src/components/Faq.jsx
@@ -37,17 +37,17 @@ const faqs = [
 const Faq = () => {
   return (
     <>
-    <div className="bg-white dark:bg-gray-900">
+    <div className="bg-white -mt-24 ">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="mx-auto max-w-4xl">
-          <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
+          <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl ">
             Frequently asked questions
           </h2>
-          <dl className="mt-16 divide-y divide-gray-900/10 dark:divide-white/10">
+          <dl className="mt-16 divide-y divide-gray-900/10 ">
             {faqs.map((faq) => (
               <Disclosure key={faq.question} as="div" className="py-6 first:pt-0 last:pb-0">
                 <dt>
-                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900 dark:text-white">
+                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
                     <span className="text-base/7 font-semibold">{faq.question}</span>
                     <span className="ml-6 flex h-7 items-center">
                       <PlusSmallIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
@@ -56,7 +56,7 @@ const Faq = () => {
                   </DisclosureButton>
                 </dt>
                 <DisclosurePanel as="dd" className="mt-2 pr-12">
-                  <p className="text-base/7 text-gray-600 dark:text-gray-400">{faq.answer}</p>
+                  <p className="text-base/7 text-gray-600 ">{faq.answer}</p>
                 </DisclosurePanel>
               </Disclosure>
             ))}
