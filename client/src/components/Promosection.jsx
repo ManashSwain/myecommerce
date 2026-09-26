bc9abaa43b0296cf7b9af058bb1153aa064970d4
ManashSwain <manash0802@gmail.com>
2026-09-26 12:51:50 +0530

    Update  Hero section Content

--- client/src/components/Promosection.jsx
+++ client/src/components/Promosection.jsx
@@ -8,11 +8,10 @@ const Promosection = () => {
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div className="sm:max-w-lg">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Summer styles are finally here
+                Made for every moment
               </h1>
               <p className="mt-4 text-xl text-gray-500">
-                This year, our new summer collection will shelter you from the
-                harsh elements of a world that doesn't care if you live or die.
+                From everyday essentials to standout pieces, find styles that make every outfit your own.
               </p>
             </div>
             <div>
